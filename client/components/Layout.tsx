046f9089a95e75f7c678ebdd4f69909405f0dd84
Angu Chen <anguchen96@gmail.com>
2025-11-21 14:00:26 +1300

    npm installed react-router-dom and cleaned up route so it works as expected.

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Outlet } from 'react-router'
+import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
   // filter states look at docs about context
--- client/routes.tsx
+++ client/routes.tsx
@@ -1,11 +1,11 @@
-import { createRoutesFromElements, Route } from 'react-router'
+import { createRoutesFromElements, Route } from 'react-router-dom'
 import App from './pages/App.tsx'
 import Home from './pages/Home.tsx'
 import Layout from './components/Layout.tsx'
 
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
-    <Route index path="/home" element={<Home />} />
-    <Route path="/app" element={<App />} />
+    <Route index element={<Home />} />
+    <Route path="app" element={<App />} />
   </Route>,
 )
