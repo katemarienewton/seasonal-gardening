f0d32426376b31259a15c4b52db0e845ce867f0d
Sabrina-Rigg <sabrinaschellkes@gmail.com>
2025-11-22 14:26:12 +1300

    set up db function file and allplants route

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,5 +1,14 @@
 import db from './connection'
 
 export async function getAllPlants() {
-  return db('vegetables').select()
+  const fruits = db('fruit')
+    .join('vegetables', 'vegetables.name', 'vegetable.image')
+    .select('fruit.name', 'fruit.image')
+
+  const vegetables = db('vegetables')
+    .join('fruit', 'fruit.name', 'fruit.image')
+    .select('vegetables.name', 'vegetables.image')
+
+  const result = await fruits.union(vegetables)
+  return result
 }
