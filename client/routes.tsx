97e92fbdf0603dfd7e0ab4d94ab3f02ad5191af4
ravidassnz <ravidassnz@gmail.com>
2025-11-24 10:04:40 +1300

    updated the routes for alll plants

--- client/routes.tsx
+++ client/routes.tsx
@@ -2,16 +2,16 @@ import { createRoutesFromElements, Route } from 'react-router'
 import App from './pages/App.tsx'
 import Home from './pages/Home.tsx'
 import Layout from './components/Layout.tsx'
-import AllPlantsPage from './components/AllPlantsPage.tsx'
+// import AllPlantsPage from './components/AllPlantsPage.tsx'
 import MyGarden from './pages/MyGarden.tsx'
 
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
     <Route path="app" element={<App />} />
-    <Route path="plant/:id" element={<PlantPage />} />
+    {/* <Route path="plant/:id" element={<PlantPage />} />
     <Route path="plant/:id/guide" element={<PlantGuide />} />
-    <Route path="plants" element={<AllPlantsPage />} />
+    <Route path="plants" element={<AllPlantsPage />} /> */}
     <Route path="my-garden" element={<MyGarden />} />
   </Route>,
 )
