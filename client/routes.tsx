aeb581bfc05431fdb5700f00cd40e2d6e83ec9dc
Sabrina-Rigg <sabrinaschellkes@gmail.com>
2025-11-24 14:57:34 +1300

    conflicts routes.tsx resolved

--- client/routes.tsx
+++ client/routes.tsx
@@ -2,18 +2,18 @@ import { createRoutesFromElements, Route } from 'react-router'
 import App from './pages/App.tsx'
 import Home from './pages/Home.tsx'
 import Layout from './components/Layout.tsx'
-// import AllPlantsPage from './components/AllPlantsPage.tsx'
+import AllPlantsPage from './components/AllPlantsPage.tsx'
 import MyGarden from './pages/MyGarden.tsx'
-import PlantPage from './components/PlantPage.tsx'
 import PlantGuide from './components/PlantGuide.tsx'
+import PlantPage from './components/PlantPage.tsx'
 
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
     <Route path="app" element={<App />} />
-    {/* <Route path="plant/:id" element={<PlantPage />} />
+    <Route path="plant/:id" element={<PlantPage />} />
     <Route path="plant/:id/guide" element={<PlantGuide />} />
-    <Route path="plants" element={<AllPlantsPage />} /> */}
+    <Route path="plants" element={<AllPlantsPage />} />
     <Route path="my-garden" element={<MyGarden />} />
   </Route>,
 )
