5215801f7cb3b4cce3c1b1e200e48ed58e023ef1
ravidassnz <ravidassnz@gmail.com>
2025-11-25 12:35:39 +1300

    created schema table for migrations

--- server/db/migrations/20251124233051_user_garden.js
+++ server/db/migrations/20251124233051_user_garden.js
@@ -2,14 +2,13 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+export function up(knex) {
+  return knex.schema.createTable('user_garden', (table) => {
+    table.increments('id')
+    table.integer('user_id').references('users.id')
+    table.integer('plant_id').references('vegetables.id')
+  })
+}
+export function down(knex) {
+  return knex.schema.dropTable('user_garden')
+}
