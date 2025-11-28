4f152eeb1586f2d19941eb140ec7f95e5bcfacd8
Rebecca Ingulkar <rebeccaingulkar@outlook.com>
2025-11-29 09:31:55 +1300

    removed import that isn't used

--- client/components/PlantCalculatorModal.tsx
+++ client/components/PlantCalculatorModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { VegetableYield } from '../../models/vegetableYield'
 import { calculatePlants } from '../apis/plantCalculator'
 
